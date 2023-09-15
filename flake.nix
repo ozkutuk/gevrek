a2b77a35d5a54fa740794cc16bd3683574b4e00d
Berk Ozkutuk <ozkutuk@protonmail.com>
2023-09-15 23:40:57 +0200

    Add lua to devShell, add formatter, format nix file

--- flake.nix
+++ flake.nix
@@ -4,13 +4,22 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
   };
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
-      imports = [ inputs.haskell-flake.flakeModule ];
+      imports = [inputs.haskell-flake.flakeModule];
 
-      perSystem = { self', pkgs, ... }: {
-
+      perSystem = {
+        self',
+        pkgs,
+        config,
+        ...
+      }: {
         # Typically, you just want a single project named "default". But
         # multiple projects are also possible, each using different GHC version.
         haskellProjects.default = {
@@ -24,11 +33,11 @@
           # Note that local packages are automatically included in `packages`
           # (defined by `defaults.packages` option).
           #
-          # packages = { 
+          # packages = {
           #   aeson.source = "1.5.0.0"; # Hackage version override
-          #   shower.source = inputs.shower; 
+          #   shower.source = inputs.shower;
           # };
-          # settings = { 
+          # settings = {
           #   aeson = {
           #     check = false;
           #   };
@@ -39,20 +48,33 @@
           # };
 
           devShell = {
-           # Enabled by default
-           enable = true;  
-          
-           # Programs you want to make available in the shell.
-           # Default programs can be disabled by setting to 'null'
-           tools = hp: {
-             fourmolu = hp.fourmolu;
-             cabal-fmt = hp.cabal-fmt;
-             ghcid = null;
-           };
-          
-           hlsCheck.enable = true;
+            # Enabled by default
+            enable = true;
+
+            # Programs you want to make available in the shell.
+            # Default programs can be disabled by setting to 'null'
+            tools = hp: {
+              fourmolu = hp.fourmolu;
+              cabal-fmt = hp.cabal-fmt;
+              ghcid = null;
+            };
+
+            hlsCheck.enable = true;
           };
+
+          autoWire = ["packages" "apps" "checks"]; # Wire all but the devShell
         };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            config.haskellProjects.default.outputs.devShell
+          ];
+          nativeBuildInputs = [
+            pkgs.lua5_4
+          ];
+        };
+
+        formatter = pkgs.alejandra;
 
         # haskell-flake doesn't set the default package, but you can do it here.
         packages.default = self'.packages.gevrek;
